3a96d67734e563ccb3442ab73474a99af1749e66
Taras <kazemir.t23.23@gmail.com>
2023-08-19 22:53:16 +0300

    chore: update webpack configuration

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,8 +4,6 @@ module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    static: {
-      directory: path.resolve(__dirname, '../dist'),
-    },
+    contentBase: path.resolve(__dirname, '../dist'),
   },
 };
